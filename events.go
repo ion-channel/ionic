7f0db9a657d7000033d5ee5ac8a0421fdf997315
Daniel Hess <dan9186@gmail.com>
2017-05-25 14:01:42 -0700

    updating names of things to read better and be more idiomatic

--- events.go
+++ events.go
@@ -3,15 +3,17 @@ package ionic
 // Event represents a singular occurance of a change within the Ion Channel
 // system that can be emmitted to trigger a notification
 type Event struct {
-	Vulns EventVulnerability `json:"vulns"`
+	Vulnerability VulnerabilityEvent `json:"vulnerability,omitempty"`
 }
 
-type EventVulnerability struct {
+// VulnerabilityEvent represents the vulnerability releated segement of an Event within Ion Channel
+type VulnerabilityEvent struct {
 	Updates  []string       `json:"updates,omitempty"`
-	Projects []EventProject `json:"projects,omitempty"`
+	Projects []ProjectEvent `json:"projects,omitempty"`
 }
 
-type EventProject struct {
+// ProjectEvent represents a project within an Event within Ion Channel
+type ProjectEvent struct {
 	Project  string   `json:"project"`
 	Org      string   `json:"org"`
 	Versions []string `json:"versions"`
@@ -19,19 +21,19 @@ type EventProject struct {
 
 // Append takes an event to join and leaves the union of the two events
 func (e *Event) Append(toAppend Event) {
-	for _, v := range toAppend.Vulns.Updates {
+	for _, v := range toAppend.Vulnerability.Updates {
 		if !e.contains(v) {
-			e.Vulns.Updates = append(e.Vulns.Updates, v)
+			e.Vulnerability.Updates = append(e.Vulnerability.Updates, v)
 		}
 	}
 }
 
 func (e *Event) contains(vuln string) bool {
-	if e.Vulns.Updates == nil {
+	if e.Vulnerability.Updates == nil {
 		return false
 	}
 
-	for _, existing := range e.Vulns.Updates {
+	for _, existing := range e.Vulnerability.Updates {
 		if existing == vuln {
 			return true
 		}
