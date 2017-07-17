ff957a81ba617d8fc796847cadeaf2beab7e405f
Daniel Hess <dan9186@gmail.com>
2017-07-16 23:20:33 -0700

    no need to append if each event is singular

--- events/events.go
+++ events/events.go
@@ -6,26 +6,3 @@ type Event struct {
 	Vulnerability *VulnerabilityEvent `json:"vulnerability,omitempty"`
 	User          *UserEvent          `json:"user,omitempty"`
 }
-
-// Append takes an event to join and leaves the union of the two events
-func (e *Event) Append(toAppend Event) {
-	for _, v := range toAppend.Vulnerability.Updates {
-		if !e.contains(v) {
-			e.Vulnerability.Updates = append(e.Vulnerability.Updates, v)
-		}
-	}
-}
-
-func (e *Event) contains(vuln string) bool {
-	if e.Vulnerability.Updates == nil {
-		return false
-	}
-
-	for _, existing := range e.Vulnerability.Updates {
-		if existing == vuln {
-			return true
-		}
-	}
-
-	return false
-}
