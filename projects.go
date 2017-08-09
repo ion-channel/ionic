8a30f64246ab56bcec8ed211cc081b00b5555249
Daniel Hess <dan9186@gmail.com>
2017-08-08 22:55:19 -0700

    adding get project

---
+++ projects.go
@@ -0,0 +1,32 @@
+package ionic
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/ion-channel/ionic/projects"
+)
+
+const (
+	getProjectEndpoint = "v1/projects/getProject"
+)
+
+func (ic *IonClient) GetProject(id, teamID string) (*projects.Project, error) {
+	params := &url.Values{}
+	params.Set("id", id)
+	params.Set("team_id", teamID)
+
+	b, err := ic.get(getProjectEndpoint, params, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get project: %v", err.Error())
+	}
+
+	var p projects.Project
+	err = json.Unmarshal(b, &p)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get project: %v", err.Error())
+	}
+
+	return &p, nil
+}
