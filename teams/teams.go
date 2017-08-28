c88d9372e3c2bcf1043c589183cfcae92cef596d
Daniel Hess <dan9186@gmail.com>
2017-08-28 15:57:49 -0700

    adding get teams

---
+++ teams/teams.go
@@ -0,0 +1,19 @@
+package teams
+
+import (
+	"time"
+)
+
+// Team is a representation of an Ion Channel Team within the system
+type Team struct {
+	ID           string    `json:"id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	Name         string    `json:"name"`
+	SysAdmin     bool      `json:"sys_admin"`
+	DeletedAt    time.Time `json:"deleted_at"`
+	POCName      string    `json:"poc_name"`
+	POCEmail     string    `json:"poc_email"`
+	POCNameHash  string    `json:"poc_name_hash"`
+	POCEmailHash string    `json:"poc_email_hash"`
+}
