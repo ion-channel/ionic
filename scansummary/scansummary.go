7295d01728a6e3f89086f8bc58c936a827ee4ea0
Daniel Hess <dan9186@gmail.com>
2017-09-19 18:09:04 -0700

    adding the xml tags to the struct

--- scansummary/scansummary.go
+++ scansummary/scansummary.go
@@ -9,18 +9,18 @@ import (
 // individual scan on a project.  It contains all the details the Ion Channel
 // platform discovers for that scan.
 type ScanSummary struct {
-	ID          string          `json:"id"`
-	TeamID      string          `json:"team_id"`
-	ProjectID   string          `json:"project_id"`
-	AnalysisID  string          `json:"analysis_id"`
-	Summary     string          `json:"summary"`
-	Results     json.RawMessage `json:"results"`
-	UpdatedAt   time.Time       `json:"updated_at"`
-	CreatedAt   time.Time       `json:"created_at"`
-	Duration    float64         `json:"duration"`
-	Passed      bool            `json:"passed"`
-	Risk        string          `json:"risk"`
-	Name        string          `json:"name"`
-	Description string          `json:"description"`
-	Type        string          `json:"type"`
+	ID          string          `json:"id" xml:"id"`
+	TeamID      string          `json:"team_id" xml:"team_id"`
+	ProjectID   string          `json:"project_id" xml:"project_id"`
+	AnalysisID  string          `json:"analysis_id" xml:"analysis_id"`
+	Summary     string          `json:"summary" xml:"summary"`
+	Results     json.RawMessage `json:"results" xml:"results"`
+	UpdatedAt   time.Time       `json:"updated_at" xml:"updated_at"`
+	CreatedAt   time.Time       `json:"created_at" xml:"created_at"`
+	Duration    float64         `json:"duration" xml:"duration"`
+	Passed      bool            `json:"passed" xml:"passed"`
+	Risk        string          `json:"risk" xml:"risk"`
+	Name        string          `json:"name" xml:"name"`
+	Description string          `json:"description" xml:"description"`
+	Type        string          `json:"type" xml:"type"`
 }
