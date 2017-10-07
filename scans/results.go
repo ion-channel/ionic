d85cb2d40b9a5220316b289d7128181d2ef3fef2
Daniel Hess <dan9186@gmail.com>
2017-10-06 23:41:37 -0700

    adding more scan results

---
+++ scans/results.go
@@ -0,0 +1,162 @@
+package scans
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// Results is an Ion Channel representation of the results from a
+// scan summary.  It contains what type of results and the data pertaining to
+// the results.
+type Results struct {
+	Type string      `json:"type" xml:"type"`
+	Data interface{} `json:"data,omitempty" xml:"data,omitempty"`
+}
+
+type results struct {
+	Type    string          `json:"type"`
+	RawData json.RawMessage `json:"data"`
+}
+
+// UnmarshalJSON is a custom JSON unmarshaller implementation for the standard
+// go json package to know how to properly interpret ScanSummaryResults from
+// JSON.
+func (r *Results) UnmarshalJSON(b []byte) error {
+	var tr results
+	err := json.Unmarshal(b, &tr)
+	if err != nil {
+		return err
+	}
+
+	r.Type = tr.Type
+
+	switch strings.ToLower(tr.Type) {
+	case "about_yml":
+		var a AboutYMLResults
+		err := json.Unmarshal(tr.RawData, &a)
+		if err != nil {
+			return err
+		}
+
+		r.Data = a
+	case "coverage", "external_coverage":
+		var c CoverageResults
+		err := json.Unmarshal(tr.RawData, &c)
+		if err != nil {
+			return err
+		}
+
+		r.Data = c
+	case "dependency":
+		var d DependencyResults
+		err := json.Unmarshal(tr.RawData, &d)
+		if err != nil {
+			return err
+		}
+
+		r.Data = d
+	case "ecosystems":
+		var e EcosystemResults
+		err := json.Unmarshal(tr.RawData, &e)
+		if err != nil {
+			return err
+		}
+
+		r.Data = e
+	case "license":
+		var l LicenseResults
+		err := json.Unmarshal(tr.RawData, &l)
+		if err != nil {
+			return err
+		}
+
+		r.Data = l
+	case "virus", "clamav":
+		var v VirusResults
+		err := json.Unmarshal(tr.RawData, &v)
+		if err != nil {
+			return err
+		}
+
+		r.Data = v
+	case "vulnerability":
+		var v VulnerabilityResults
+		err := json.Unmarshal(tr.RawData, &v)
+		if err != nil {
+			return err
+		}
+
+		r.Data = v
+	default:
+		return fmt.Errorf("invalid results type")
+	}
+
+	return nil
+}
+
+type AboutYMLResults struct {
+	Message string `json:"message" xml:"message"`
+	Valid   bool   `json:"valid" xml:"valid"`
+	Content string `json:"content" xml:"content"`
+}
+
+type CoverageResults struct {
+}
+
+type DependencyResults struct {
+	Dependencies []struct {
+		LatestVersion string `json:"latest_version"`
+		Org           string `json:"org"`
+		Name          string `json:"name"`
+		Type          string `json:"type"`
+		Package       string `json:"package"`
+		Version       string `json:"version"`
+		Scope         string `json:"scope"`
+	} `json:"dependencies"`
+	Meta struct {
+		FirstDegreeCount     int `json:"first_degree_count"`
+		NoVersionCount       int `json:"no_version_count"`
+		TotalUniqueCount     int `json:"total_unique_count"`
+		UpdateAvailableCount int `json:"update_available_count"`
+	} `json:"meta"`
+}
+
+type EcosystemResults struct {
+	Ecosystems []struct {
+		Ecosystem string `json:"ecosystem" xml:"ecosystem"`
+		Lines     int    `json:"lines" xml:"lines"`
+	} `json:"ecosystems" xml:"ecosystems"`
+}
+
+type LicenseResults struct {
+	License struct {
+		Name string `json:"name"`
+		Type []struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"license"`
+}
+
+type VirusResults struct {
+	KnownViruses       int      `json:"known_viruses" xml:"known_viruses"`
+	EngineVersion      string   `json:"engine_version" xml:"engine_version"`
+	ScannedDirectories int      `json:"scanned_directories" xml:"scanned_directories"`
+	ScannedFiles       int      `json:"scanned_files" xml:"scanned_files"`
+	InfectedFiles      int      `json:"infected_files" xml:"infected_files"`
+	DataScanned        string   `json:"data_scanned" xml:"data_scanned"`
+	DataRead           string   `json:"data_read" xml:"data_read"`
+	Time               string   `json:"time" xml:"time"`
+	FileNotes          struct{} `json:"file_notes" xml:"file_notes"`
+	ClamavDetails      struct {
+		ClamavVersion   string `json:"clamav_version" xml:"clamav_version"`
+		ClamavDbVersion string `json:"clamav_db_version" xml:"clamav_db_version"`
+	} `json:"clam_av_details" xml:"clam_av_details"`
+}
+
+type VulnerabilityResults struct {
+	Vulnerabilities []interface{} `json:"vulnerabilities"`
+	Meta            struct {
+		VulnerabilityCount int `json:"vulnerability_count"`
+	} `json:"meta"`
+}
---
+++ scans/results_test.go
@@ -0,0 +1,84 @@
+package scans
+
+import (
+	"encoding/json"
+	"testing"
+
+	. "github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+)
+
+func TestScanResults(t *testing.T) {
+	g := Goblin(t)
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+
+	g.Describe("Scan Results", func() {
+		g.It("should unmarshal a scan results with about yml data", func() {
+			var r Results
+			err := json.Unmarshal([]byte(SampleValidScanResultsAboutYML), &r)
+
+			Expect(err).To(BeNil())
+			Expect(r.Type).To(Equal("about_yml"))
+
+			a, ok := r.Data.(AboutYMLResults)
+			Expect(ok).To(Equal(true))
+			Expect(a.Content).To(Equal("some content"))
+		})
+
+		g.It("should unmarshal a scan results with dependency data", func() {
+			var r Results
+			err := json.Unmarshal([]byte(SampleValidScanResultsDependency), &r)
+
+			Expect(err).To(BeNil())
+			Expect(r.Type).To(Equal("dependency"))
+
+			d, ok := r.Data.(DependencyResults)
+			Expect(ok).To(Equal(true))
+			Expect(len(d.Dependencies)).To(Equal(7))
+			Expect(d.Meta.FirstDegreeCount).To(Equal(3))
+			Expect(d.Meta.NoVersionCount).To(Equal(0))
+			Expect(d.Meta.TotalUniqueCount).To(Equal(7))
+			Expect(d.Meta.UpdateAvailableCount).To(Equal(2))
+		})
+
+		g.It("should unmarshal a scan results with ecosystem data", func() {
+			var r Results
+			err := json.Unmarshal([]byte(SampleValidScanResultsEcosystems), &r)
+
+			Expect(err).To(BeNil())
+			Expect(r.Type).To(Equal("ecosystems"))
+
+			e, ok := r.Data.(EcosystemResults)
+			Expect(ok).To(Equal(true))
+			Expect(len(e.Ecosystems)).To(Equal(3))
+		})
+
+		g.It("should unmarshal a scan results with license data", func() {
+			var r Results
+			err := json.Unmarshal([]byte(SampleValidScanResultsLicense), &r)
+
+			Expect(err).To(BeNil())
+			Expect(r.Type).To(Equal("license"))
+
+			l, ok := r.Data.(LicenseResults)
+			Expect(ok).To(Equal(true))
+			Expect(l.License.Name).To(Equal("Not found"))
+		})
+
+		g.It("should return an error for an invalid results type", func() {
+			var r Results
+			err := json.Unmarshal([]byte(SampleInvalidResults), &r)
+
+			Expect(err).NotTo(BeNil())
+			Expect(err.Error()).To(ContainSubstring("invalid results type"))
+		})
+	})
+}
+
+const (
+	SampleValidScanResultsAboutYML   = `{"type":"about_yml", "data":{"message": "foo message", "valid": true, "content": "some content"}}`
+	SampleValidScanResultsDependency = `{"type":"dependency","data":{"dependencies":[{"latest_version":"2.0","org":"net.sourceforge.javacsv","name":"javacsv","type":"maven","package":"jar","version":"2.0","scope":"compile"},{"latest_version":"4.12","org":"junit","name":"junit","type":"maven","package":"jar","version":"4.11","scope":"test"},{"latest_version":"1.4-atlassian-1","org":"org.hamcrest","name":"hamcrest-core","type":"maven","package":"jar","version":"1.3","scope":"test"},{"latest_version":"4.5.2","org":"org.apache.httpcomponents","name":"httpclient","type":"maven","package":"jar","version":"4.3.4","scope":"compile"},{"latest_version":"4.4.5","org":"org.apache.httpcomponents","name":"httpcore","type":"maven","package":"jar","version":"4.3.2","scope":"compile"},{"latest_version":"99.0-does-not-exist","org":"commons-logging","name":"commons-logging","type":"maven","package":"jar","version":"1.1.3","scope":"compile"},{"latest_version":"20041127.091804","org":"commons-codec","name":"commons-codec","type":"maven","package":"jar","version":"1.6","scope":"compile"}],"meta":{"first_degree_count":3,"no_version_count":0,"total_unique_count":7,"update_available_count":2}}}`
+	SampleValidScanResultsEcosystems = `{"type":"ecosystems","data":{"ecosystems":[{"ecosystem":"Java","lines":2430},{"ecosystem":"Makefile","lines":210},{"ecosystem":"Ruby","lines":666}]}}`
+	SampleValidScanResultsLicense    = `{"type":"license","data":{"license":{"name":"Not found","type":[]}}}`
+	SampleInvalidResults             = `{"type":"fooresult", "data":"I pitty the foo"}`
+)
