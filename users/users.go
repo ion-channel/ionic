fbf396a4e58d4458a0e4aea8597e2b1a9fc5ba80
Daniel Hess <dan9186@gmail.com>
2017-11-15 14:16:37 -0800

    adding time related fields to user

--- users/users.go
+++ users/users.go
@@ -1,10 +1,17 @@
 package users
 
+import (
+	"time"
+)
+
 // User is a representation of an Ion Channel User within the system
 type User struct {
-	ID         string `json:"id"`
-	Email      string `json:"email"`
-	Username   string `json:"username"`
-	ChatHandle string `json:"chat_handle"`
-	SysAdmin   bool   `json:"sys_admin"`
+	ID         string    `json:"id"`
+	Email      string    `json:"email"`
+	Username   string    `json:"username"`
+	ChatHandle string    `json:"chat_handle"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	LastActive time.Time `json:"last_active_at"`
+	SysAdmin   bool      `json:"sys_admin"`
 }
