8fae812f940ed3412a161a291da15fa9dcd3124e
Daniel Hess <dan9186@gmail.com>
2017-11-15 14:26:21 -0800

    adding teams field to user

--- users/users.go
+++ users/users.go
@@ -6,13 +6,14 @@ import (
 
 // User is a representation of an Ion Channel User within the system
 type User struct {
-	ID         string    `json:"id"`
-	Email      string    `json:"email"`
-	Username   string    `json:"username"`
-	ChatHandle string    `json:"chat_handle"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
-	LastActive time.Time `json:"last_active_at"`
-	Metadata   string    `json:"metadata"`
-	SysAdmin   bool      `json:"sys_admin"`
+	ID         string            `json:"id"`
+	Email      string            `json:"email"`
+	Username   string            `json:"username"`
+	ChatHandle string            `json:"chat_handle"`
+	CreatedAt  time.Time         `json:"created_at"`
+	UpdatedAt  time.Time         `json:"updated_at"`
+	LastActive time.Time         `json:"last_active_at"`
+	Metadata   string            `json:"metadata"`
+	SysAdmin   bool              `json:"sys_admin"`
+	Teams      map[string]string `json:"teams"`
 }
