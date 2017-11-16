7dacb5eb596963a2f681afa8c89842e5fa519798
Daniel Hess <dan9186@gmail.com>
2017-11-15 23:12:25 -0800

    adding missing deleted at field

--- teams/teams.go
+++ teams/teams.go
@@ -9,6 +9,7 @@ type Team struct {
 	ID        string    `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 	Name      string    `json:"name"`
 	SysAdmin  bool      `json:"sys_admin"`
 	POCName   string    `json:"poc_name"`
