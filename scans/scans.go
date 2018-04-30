fde009f35fd1399238877505fb69ffafaf44f0c1
Daniel Hess <dan9186@gmail.com>
2018-04-30 09:08:12 -0700

    Adding ability to handle both result types

--- scans/scans.go
+++ scans/scans.go
@@ -1,20 +1,88 @@
 package scans
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
 	"time"
 )
 
 // Scan represents the data collected from an individual scan on a project.
 type Scan struct {
-	ID          string             `json:"id"`
-	TeamID      string             `json:"team_id"`
-	ProjectID   string             `json:"project_id"`
-	AnalysisID  string             `json:"analysis_id"`
-	Summary     string             `json:"summary"`
-	Results     *TranslatedResults `json:"results"`
-	CreatedAt   time.Time          `json:"created_at"`
-	UpdatedAt   time.Time          `json:"updated_at"`
-	Duration    float64            `json:"duration"`
-	Name        string             `json:"name"`
-	Description string             `json:"description"`
+	*scan
+	TranslatedResults   *TranslatedResults   `json:"-"`
+	UntranslatedResults *UntranslatedResults `json:"-"`
+}
+
+type scan struct {
+	ID          string          `json:"id"`
+	TeamID      string          `json:"team_id"`
+	ProjectID   string          `json:"project_id"`
+	AnalysisID  string          `json:"analysis_id"`
+	Summary     string          `json:"summary"`
+	Results     json.RawMessage `json:"results"`
+	CreatedAt   time.Time       `json:"created_at"`
+	UpdatedAt   time.Time       `json:"updated_at"`
+	Duration    float64         `json:"duration"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+}
+
+// MarshalJSON meets the marshaller interface to custom wrangle translated or
+// untranslated results into the same results key for the JSON
+func (s *Scan) MarshalJSON() ([]byte, error) {
+	if s.TranslatedResults != nil {
+		b, err := json.Marshal(s.TranslatedResults)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal translated results: %v", err.Error())
+		}
+
+		s.Results = b
+	}
+
+	if s.UntranslatedResults != nil {
+		b, err := json.Marshal(s.UntranslatedResults)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal untranslated results: %v", err.Error())
+		}
+
+		s.Results = b
+	}
+
+	return json.Marshal(s.scan)
+}
+
+// UnmarshalJSON meets the unmarshaller interface to custom wrangle results from
+// a singular results key into the correct translated or untranslated results
+// field
+func (s *Scan) UnmarshalJSON(b []byte) error {
+	var ss scan
+	err := json.Unmarshal(b, &ss)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal scans: %v", err.Error())
+	}
+
+	s.scan = &ss
+
+	var tr TranslatedResults
+	err = json.Unmarshal(s.Results, &tr)
+	if err != nil {
+		if strings.Contains(err.Error(), "unsupported results type found") {
+			var un UntranslatedResults
+			err := json.Unmarshal(s.Results, &un)
+			if err != nil {
+				return fmt.Errorf("failed to unmarshal untranslated results: %v", err.Error())
+			}
+
+			s.UntranslatedResults = &un
+
+			return nil
+		}
+
+		return fmt.Errorf("failed to unmarshal translated results: %v", err.Error())
+	}
+
+	s.TranslatedResults = &tr
+
+	return nil
 }
