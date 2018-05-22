23bd36f965a6338d579ec062f754acbda28bb49e
Daniel Hess <dan9186@gmail.com>
2018-05-22 16:06:01 -0700

    adding new summary constructor

---
+++ analysis/analysis_test.go
@@ -0,0 +1,33 @@
+package analysis
+
+import (
+	"testing"
+
+	"github.com/ion-channel/ionic/rulesets"
+
+	. "github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+)
+
+func TestAnalysis(t *testing.T) {
+	g := Goblin(t)
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+
+	g.Describe("New Analysis Summary", func() {
+		g.It("should return a new analysis summary", func() {
+			expectedAnalysisID := "analysizing"
+
+			a := &Analysis{
+				ID: expectedAnalysisID,
+			}
+			ar := &rulesets.AppliedRulesetSummary{}
+
+			s := NewSummary(a, ar)
+			Expect(s.ID).To(Equal(expectedAnalysisID))
+			Expect(s.AnalysisID).To(Equal(expectedAnalysisID))
+			Expect(s.RulesetName).To(Equal("N/A"))
+			Expect(s.Passed).To(Equal(false))
+			Expect(s.Risk).To(Equal("high"))
+		})
+	})
+}
