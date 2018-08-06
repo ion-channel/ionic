7bf8ea540a27eeff522a90bf509a045408e64722
Daniel Miles <daniel.miles@ionchannel.io>
2018-08-06 16:16:05 -0700

    changing strings in a projects/project to string poitners

--- projects/projects.go
+++ projects/projects.go
@@ -9,25 +9,25 @@ import (
 
 //Project is a representation of a project within the Ion Channel system
 type Project struct {
-	ID             string          `json:"id"`
-	TeamID         string          `json:"team_id"`
-	RulesetID      string          `json:"ruleset_id"`
-	Name           string          `json:"name"`
-	Type           string          `json:"type"`
-	Source         string          `json:"source"`
-	Branch         string          `json:"branch"`
-	Description    string          `json:"description"`
+	ID             *string          `json:"id"`
+	TeamID         *string          `json:"team_id"`
+	RulesetID      *string          `json:"ruleset_id"`
+	Name           *string          `json:"name"`
+	Type           *string          `json:"type"`
+	Source         *string          `json:"source"`
+	Branch         *string          `json:"branch"`
+	Description    *string          `json:"description"`
 	Active         bool            `json:"active"`
-	ChatChannel    string          `json:"chat_channel"`
+	ChatChannel    *string          `json:"chat_channel"`
 	CreatedAt      time.Time       `json:"created_at"`
 	UpdatedAt      time.Time       `json:"updated_at"`
-	DeployKey      string          `json:"deploy_key"`
+	DeployKey      *string          `json:"deploy_key"`
 	Monitor        bool            `json:"should_monitor"`
-	POCName        string          `json:"poc_name"`
-	POCEmail       string          `json:"poc_email"`
-	Username       string          `json:"username"`
-	Password       string          `json:"password"`
-	KeyFingerprint string          `json:"key_fingerprint"`
+	POCName        *string          `json:"poc_name"`
+	POCEmail       *string          `json:"poc_email"`
+	Username       *string          `json:"username"`
+	Password       *string          `json:"password"`
+	KeyFingerprint *string          `json:"key_fingerprint"`
 	Aliases        []aliases.Alias `json:"aliases"`
 	Tags           []tags.Tag      `json:"tags"`
 }
