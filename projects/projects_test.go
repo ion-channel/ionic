3ed9f8e44d49519b9e5c203bd4ae07d4d3826a45
Daniel Hess <dan9186@gmail.com>
2018-08-07 15:11:31 -0700

    another test case of blank is valid

--- projects/projects_test.go
+++ projects/projects_test.go
@@ -23,10 +23,25 @@ func TestAnalysis(t *testing.T) {
 			Expect(len(fs)).To(Equal(0))
 		})
 
-		g.It("should return a list of fields and an error if a project is invalid", func() {
+		g.It("should return no errors for a blank field", func() {
+			var p Project
+			err := json.Unmarshal([]byte(sampleValidBlankProject), &p)
+			Expect(err).To(BeNil())
+			Expect(p.ID).NotTo(BeNil())
+			Expect(*p.ID).To(Equal(""))
+
+			fs, err := p.Validate()
+			Expect(err).To(BeNil())
+			Expect(len(fs)).To(Equal(0))
+		})
+
+		g.It("should return missing fields as a list and error", func() {
 			var p Project
 			err := json.Unmarshal([]byte(sampleInvalidProject), &p)
 			Expect(err).To(BeNil())
+			Expect(p.Name).To(BeNil())
+			Expect(p.Type).To(BeNil())
+			Expect(p.Branch).To(BeNil())
 
 			fs, err := p.Validate()
 			Expect(err).To(Equal(ErrInvalidProject))
@@ -39,6 +54,7 @@ func TestAnalysis(t *testing.T) {
 }
 
 const (
-	sampleValidProject   = `{"id":"someid","team_id":"someteamid","ruleset_id":"someruleset","name":"coolproject","type":"git","source":"github","branch":"master","description":"the coolest project around","active":true,"chat_channel":"#thechan","created_at":"2018-08-07T13:42:47.258415155-07:00","updated_at":"2018-08-07T13:42:47.258415176-07:00","deploy_key":"thekey","should_monitor":false,"poc_name":"youknowit","poc_email":"you@know.it","username":"knowit","password":"supersecret","key_fingerprint":"supersecret","aliases":null,"tags":null}`
-	sampleInvalidProject = `{"id":"someid","team_id":"someteamid","ruleset_id":"someruleset","source":"github","description":"the coolest project around","active":true,"chat_channel":"#thechan","created_at":"2018-08-07T13:46:06.529187652-07:00","updated_at":"2018-08-07T13:46:06.529187674-07:00","deploy_key":"thekey","should_monitor":false,"poc_name":"youknowit","poc_email":"you@know.it","username":"knowit","password":"supersecret","key_fingerprint":"supersecret","aliases":null,"tags":null}`
+	sampleValidProject      = `{"id":"someid","team_id":"someteamid","ruleset_id":"someruleset","name":"coolproject","type":"git","source":"github","branch":"master","description":"the coolest project around","active":true,"chat_channel":"#thechan","created_at":"2018-08-07T13:42:47.258415155-07:00","updated_at":"2018-08-07T13:42:47.258415176-07:00","deploy_key":"thekey","should_monitor":false,"poc_name":"youknowit","poc_email":"you@know.it","username":"knowit","password":"supersecret","key_fingerprint":"supersecret","aliases":null,"tags":null}`
+	sampleInvalidProject    = `{"id":"someid","team_id":"someteamid","ruleset_id":"someruleset","source":"github","description":"the coolest project around","active":true,"chat_channel":"#thechan","created_at":"2018-08-07T13:46:06.529187652-07:00","updated_at":"2018-08-07T13:46:06.529187674-07:00","deploy_key":"thekey","should_monitor":false,"poc_name":"youknowit","poc_email":"you@know.it","username":"knowit","password":"supersecret","key_fingerprint":"supersecret","aliases":null,"tags":null}`
+	sampleValidBlankProject = `{"id":"","team_id":"someteamid","ruleset_id":"someruleset","name":"coolproject","type":"git","source":"github","branch":"master","description":"the coolest project around","active":true,"chat_channel":"#thechan","created_at":"2018-08-07T13:42:47.258415155-07:00","updated_at":"2018-08-07T13:42:47.258415176-07:00","deploy_key":"thekey","should_monitor":false,"poc_name":"youknowit","poc_email":"you@know.it","username":"knowit","password":"supersecret","key_fingerprint":"supersecret","aliases":null,"tags":null}`
 )
