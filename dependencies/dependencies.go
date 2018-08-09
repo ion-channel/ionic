2d3b7a512c4340b8a706a4b6803053f7210bd83c
Daniel Hess <dan9186@gmail.com>
2018-08-09 15:42:51 -0700

    missed a lint

--- dependencies/dependencies.go
+++ dependencies/dependencies.go
@@ -1,5 +1,7 @@
 package dependencies
 
+// Dependency represents all the known information for a dependency object
+// within the Ion Channel API
 type Dependency struct {
 	Name    string `json:"name,omitempty"`
 	Version string `json:"version"`
