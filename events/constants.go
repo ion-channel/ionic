35535b1cd8888799bda2dd39b7e4ca6a26a47b66
kellyp <kellyplummer@mac.com>
2018-09-06 15:35:43 -0700

    using constants for event action maps

---
+++ events/constants.go
@@ -0,0 +1,52 @@
+package events
+
+const (
+	// AnalysisFailed constant for analysis failed event action
+	AnalysisFailed = "analysis_failed"
+	// AnalysisFinished constant for analysis finished event action
+	AnalysisFinished = "analysis_finished"
+	// AnalysisPassed constant for analysis passed event action
+	AnalysisPassed = "analysis_passed"
+	// DeliveryFailed -
+	DeliveryFailed = "delivery_failed"
+	// ArtifactDeliveryFailed -
+	ArtifactDeliveryFailed = "artifact_delivery_failed"
+	// ReportDeliveryFailed -
+	ReportDeliveryFailed = "report_delivery_failed"
+	// SevaDeliveryFailed -
+	SevaDeliveryFailed = "seva_delivery_failed"
+	// DeliveryFinished -
+	DeliveryFinished = "delivery_finished"
+	// ArtifactDeliveryFinished -
+	ArtifactDeliveryFinished = "artifact_delivery_finished"
+	// ReportDeliveryFinished -
+	ReportDeliveryFinished = "report_delivery_finished"
+	// SevaDeliveryFinished -
+	SevaDeliveryFinished = "seva_delivery_finished"
+	// DeliveryCanceled -
+	DeliveryCanceled = "delivery_canceled"
+	// ArtifactDeliveryCanceled -
+	ArtifactDeliveryCanceled = "artifact_delivery_canceled"
+	// ReportDeliveryCanceled -
+	ReportDeliveryCanceled = "report_delivery_canceled"
+	// SevaDeliveryCanceled -
+	SevaDeliveryCanceled = "seva_delivery_canceled"
+	// ProjectAdded -
+	ProjectAdded = "project_added"
+	// VersionAdded -
+	VersionAdded = "version_added"
+	// VulnerablityAdded -
+	VulnerablityAdded = "vulnerablity_added"
+	// AccountCreated -
+	AccountCreated = "account_created"
+	// ForgotPassword -
+	ForgotPassword = "forgot_password"
+	// PasswordChanged -
+	PasswordChanged = "password_changed"
+	// UserSignup -
+	UserSignup = "user_signup"
+	// VulnerabilityAdded -
+	VulnerabilityAdded = "vulnerability_added"
+	// VulnerabilityUpdated -
+	VulnerabilityUpdated = "vulnerability_updated"
+)
