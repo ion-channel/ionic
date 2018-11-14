202d44ee9611178fbdb84e7fb4cc615af11dd065
curtis mcallister <curtis.mcallister@ionchannel.io>
2018-11-13 16:35:11 -0800

    remove unused Rule struct members

--- rules/rules.go
+++ rules/rules.go
@@ -11,8 +11,6 @@ type Rule struct {
 	Name           string    `json:"name"`
 	Description    string    `json:"description"`
 	Category       string    `json:"category"`
-	PolicyURL      string    `json:"policy_url"`
-	RemediationURL string    `json:"remediation_url"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
 }
