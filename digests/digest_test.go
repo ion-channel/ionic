37a193db2c5783ce96cb67c9ba8cf5a9f5eef89c
Daniel Hess <dan9186@gmail.com>
2019-02-07 16:42:54 -0800

    adding consideration for pending and errors

--- digests/digest_test.go
+++ digests/digest_test.go
@@ -4,6 +4,7 @@ import (
 	"sort"
 	"testing"
 
+	"github.com/ion-channel/ionic/scanner"
 	"github.com/ion-channel/ionic/scans"
 
 	"github.com/franela/goblin"
@@ -17,30 +18,41 @@ func TestDigest(t *testing.T) {
 	g.Describe("Digest", func() {
 		g.Describe("Constructor", func() {
 			g.It("should return an error for an unsupported digest type", func() {
+				s := &scanner.ScanStatus{}
 				e := scans.NewEval()
-				ds, err := NewFromEval(0, "A title", "badtype", 10, e)
+				ds, err := NewDigest(0, "A title", "badtype", 10, e, s)
 				Expect(err).To(Equal(ErrUnsupportedType))
 				Expect(ds).To(BeNil())
 			})
 
 			g.It("should return an error when the value doesn't match the type", func() {
+				s := &scanner.ScanStatus{}
 				e := scans.NewEval()
-				ds, err := NewFromEval(0, "Another title", "bool", "not a bool", e)
+				ds, err := NewDigest(0, "Another title", "bool", "not a bool", e, s)
 				Expect(err).To(Equal(ErrFailedValueAssertion))
 				Expect(ds).To(BeNil())
 
+				s = &scanner.ScanStatus{}
 				e = scans.NewEval()
-				ds, err = NewFromEval(1, "Another title", "count", "not a count", e)
+				ds, err = NewDigest(1, "Another title", "count", "not a count", e, s)
 				Expect(err).To(Equal(ErrFailedValueAssertion))
 				Expect(ds).To(BeNil())
 
+				s = &scanner.ScanStatus{}
 				e = scans.NewEval()
-				ds, err = NewFromEval(2, "Another title", "list", "not a list", e)
+				ds, err = NewDigest(2, "Another title", "list", "not a list", e, s)
 				Expect(err).To(Equal(ErrFailedValueAssertion))
 				Expect(ds).To(BeNil())
 
+				s = &scanner.ScanStatus{}
 				e = scans.NewEval()
-				ds, err = NewFromEval(3, "Another title", "percent", "not a percent", e)
+				ds, err = NewDigest(3, "Another title", "percent", "not a percent", e, s)
+				Expect(err).To(Equal(ErrFailedValueAssertion))
+				Expect(ds).To(BeNil())
+
+				s = &scanner.ScanStatus{}
+				e = scans.NewEval()
+				ds, err = NewDigest(3, "Another title", "chars", true, e, s)
 				Expect(err).To(Equal(ErrFailedValueAssertion))
 				Expect(ds).To(BeNil())
 			})
@@ -66,6 +78,42 @@ func TestDigest(t *testing.T) {
 				Expect(ds[1].Index).To(Equal(1))
 				Expect(ds[2].Index).To(Equal(2))
 				Expect(ds[3].Index).To(Equal(3))
+			})
+		})
+
+		g.Describe("States", func() {
+			g.It("should be marked as pending when no status is provided", func() {
+				e := scans.NewEval()
+				ds, err := NewDigest(0, "Another title", "chars", "some thing", e, nil)
+				Expect(err).To(BeNil())
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Pending).To(BeTrue())
+			})
+
+			g.It("should show an error if present", func() {
+				s := &scanner.ScanStatus{
+					Status:  "errored",
+					Message: "failed to perform the scan for a reason",
+				}
+				e := scans.NewEval()
+				ds, err := NewDigest(0, "Another title", "chars", "some thing", e, s)
+				Expect(err).To(BeNil())
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Pending).To(BeFalse())
+				Expect(ds.Errored).To(BeTrue())
+				Expect(ds.ErroredMessage).To(Equal("failed to perform the scan for a reason"))
+
+				s = &scanner.ScanStatus{
+					Status:  "finished",
+					Message: "completed scan",
+				}
+				e = scans.NewEval()
+				ds, err = NewDigest(0, "Another title", "chars", "some thing", e, s)
+				Expect(err).To(BeNil())
+				Expect(ds).NotTo(BeNil())
+				Expect(ds.Pending).To(BeFalse())
+				Expect(ds.Errored).To(BeFalse())
+				Expect(ds.ErroredMessage).To(Equal(""))
 			})
 		})
 	})
