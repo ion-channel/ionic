7fa6d68f81391fc50f13a38b06f1a20a8d9aae7b
kellyp <kellyplummer@mac.com>
2019-05-02 10:30:11 -0700

    produce better details for a delivery

--- scanner/deliveries.go
+++ scanner/deliveries.go
@@ -4,6 +4,22 @@ import (
 	"time"
 )
 
+const (
+	// DeliveryStatusErrored denotes a request for delivery has errored during
+	// the run, the message field will have more details
+	DeliveryStatusErrored = "errored"
+	// DeliveryStatusFinished denotes a request for delivery has been
+	// completed, view the passed field from an Delivery and the scan details for
+	// more information
+	DeliveryStatusFinished = "finished"
+	// DeliveryStatusFailed denotes a request for delivery has failed to
+	// run, the message field will have more details
+	DeliveryStatusFailed = "failed"
+	// DeliveryStatusNotConfigured denotes a request for delivery has been
+	// rejected due to no delivery details
+	DeliveryStatusNotConfigured = "not_configured"
+)
+
 // Delivery represents the delivery information of a singular artifact
 // associated with an analysis status
 type Delivery struct {
@@ -14,6 +30,7 @@ type Delivery struct {
 	Destination string    `json:"destination"`
 	Status      string    `json:"status"`
 	Filename    string    `json:"filename"`
+	Message     string    `json:"message"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
