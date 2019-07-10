4e99c91cb6139e75302d05d184f39deec90feac5
Brent Johnson <brentj433@gmail.com>
2019-07-10 10:38:10 -0700

    adds string method to Analyses object

--- teams/teams_test.go
+++ teams/teams_test.go
@@ -31,9 +31,7 @@ func TestTeam(t *testing.T) {
 				POCName:    "youknowit",
 				POCEmail:   "you@know.it",
 			}
-
 			Expect(fmt.Sprintf("%v", t)).To(Equal(`{"id":"someid","created_at":"2018-07-07T13:42:47.651387237Z","updated_at":"2018-07-07T13:42:47.651387237Z","deleted_at":"2018-07-07T13:42:47.651387237Z","name":"somename","delivering":false,"sys_admin":true,"poc_name":"youknowit","poc_email":"you@know.it"}`))
-
 		})
 	})
 }
