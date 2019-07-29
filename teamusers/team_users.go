5d38aa020c377b3235984d68609432b56c8f9134
Brent Johnson <brentj433@gmail.com>
2019-07-29 14:47:57 -0700

    adds additional constants for team users

--- teamusers/team_users.go
+++ teamusers/team_users.go
@@ -13,6 +13,17 @@ const (
 	TeamsUpdateTeamUserEndpoint = "v1/teamUsers/updateTeamUser"
 	// TeamsDeleteTeamUserEndpoint is a string representation of the current endpoint for deleting team user
 	TeamsDeleteTeamUserEndpoint = "v1/teamUsers/deleteTeamUser"
+
+	// TODO: The following endpoints will need to have functions attached to them
+
+	// TeamsInviteTeamUserEndpoint is a string representation of the current endpoint for Inviting a team user
+	TeamsInviteTeamUserEndpoint = "v1/teamUsers/inviteTeamUser"
+	// TeamsAcceptInviteTeamUserEndpoint is a string representation of the current endpoint for accepting a team user invite
+	TeamsAcceptInviteTeamUserEndpoint = "v1/teamUsers/acceptInvite"
+	// TeamsGetInviteTeamUserEndpoint is a string representation of the current endpoint for inviting a team user
+	TeamsGetInviteTeamUserEndpoint = "v1/teamUsers/getInvite"
+	// TeamsResendInviteTeamUserEndpoint is a string representation of the current endpoint for resending invite to team user
+	TeamsResendInviteTeamUserEndpoint = "v1/teamUsers/resendInvite"
 )
 
 // TeamUser is a representation of an Ion Channel Team User relationship within the system
