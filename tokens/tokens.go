dbd26275e01f5af2fb128b27143c8ece776686e3
Brent Johnson <brentj433@gmail.com>
2019-07-29 14:40:55 -0700

    adds new package tokens with constants for documention

---
+++ tokens/tokens.go
@@ -0,0 +1,12 @@
+package tokens
+
+const (
+	// TODO: the following endpoints need functions attached to them
+
+	// TokensCreateTokenEndpoint is a string representation of the current endpoint for creating a token
+	TokensCreateTokenEndpoint = "v1/tokens/createToken"
+	// TokensValidateTokenEndpoint is a string representation of the current endpoint for validating a token
+	TokensValidateTokenEndpoint = "v1/tokens/validateToken"
+	// TokensGetTokensEndpoint is a string representation of the current endpoint for getting tokens
+	TokensGetTokensEndpoint = "v1/tokens/getTokens"
+)
