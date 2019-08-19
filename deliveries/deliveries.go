33106d7e18551cd7aaa6e253f983b5cb81dd42e4
kellyp <kellyplummer@mac.com>
2019-08-19 13:17:41 -0700

    shorten name

--- deliveries/deliveries.go
+++ deliveries/deliveries.go
@@ -24,8 +24,8 @@ type Destination struct {
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
 }
 
-// CreateDestinationInput is an input representation of a single location that a team can deliver results to.
-type CreateDestinationInput struct {
+// CreateDestination is an input representation of a single location that a team can deliver results to.
+type CreateDestination struct {
 	Destination
 	AccessKey string `json:"access_key"`
 	SecretKey string `json:"secret_key"`
