a5db42573ce64105f54160b22c57e92785e03f93
Brent Johnson <brentj433@gmail.com>
2019-10-07 16:32:51 -0700

    adds GetRawVulnerabilityList

--- portfolios/portfolios.go
+++ portfolios/portfolios.go
@@ -3,6 +3,8 @@ package portfolios
 const (
 	// VulnerabilityStatsEndpoint is a string representation of the current endpoint for getting vulnerability statistics
 	VulnerabilityStatsEndpoint = "v1/animal/getVulnerabilityStats"
+	// VulnerabilityListEndpoint is a string representation for getting a list of vulnerabilities by type.
+	VulnerabilityListEndpoint = "v1/animal/getVulnerabilityList"
 )
 
 // VulnerabilityStat represents the vulnerabiity stat summary for the portfolio page
@@ -10,4 +12,11 @@ type VulnerabilityStat struct {
 	TotalVulnerabilities      int    `json:"total_vulnerabilities"`
 	UniqueVulnerabilities     int    `json:"unique_vulnerabilities"`
 	MostFrequentVulnerability string `json:"most_frequent_vulnerability"`
+}
+
+// VulnerabilityListParams represents the vulnerability list paramaters
+type VulnerabilityListParams struct {
+	Ids      []string `json:"ids"`
+	ListType string   `json:"list_type"`
+	Limit    string   `json:"limit"`
 }
