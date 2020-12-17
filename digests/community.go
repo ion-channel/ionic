ffed80a3fcc9126fe0c1e473d4f965f842631316
struthj2 <struthjoseph@gmail.com>
2020-12-17 14:29:22 -0800

    [ION-575] update community results to include last updated time
    updating for community digest, handle legacy scans

--- digests/community.go
+++ digests/community.go
@@ -9,8 +9,8 @@ import (
 
 func communityDigests(status *scanner.ScanStatus, eval *scans.Evaluation) ([]Digest, error) {
 	digests := make([]Digest, 0)
-
 	d := NewDigest(status, UniqueCommittersIndex, "unique committer", "unique committers")
+	activityDigest := NewDigest(status, CommittedAtIndex, "committed at", "committed at")
 
 	if eval != nil && !status.Errored() {
 		b, ok := eval.TranslatedResults.Data.(scans.CommunityResults)
@@ -31,5 +31,27 @@ func communityDigests(status *scanner.ScanStatus, eval *scans.Evaluation) ([]Dig
 
 	digests = append(digests, *d)
 
+	if eval != nil && !status.Errored() {
+		b, ok := eval.TranslatedResults.Data.(scans.CommunityResults)
+		if !ok {
+			return nil, fmt.Errorf("error coercing evaluation translated results into community")
+		}
+
+		activityDigest.MarshalSourceData(b, "committed at")
+
+		var evalTime string
+		evalTime = b.CommittedAt.String()
+		// check for legacy scans (as they won't have this data availible), otherwise display last time committed at
+		if b.CommittedAt.IsZero() {
+			evalTime = "N/A"
+		}
+
+		err := activityDigest.AppendEval(eval, "chars", evalTime)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create committed at digest: %v", err.Error())
+		}
+	}
+
+	digests = append(digests, *activityDigest)
 	return digests, nil
 }
--- digests/community_test.go
+++ digests/community_test.go
@@ -1,7 +1,9 @@
 package digests
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/ion-channel/ionic/scanner"
 	"github.com/ion-channel/ionic/scans"
@@ -19,21 +21,27 @@ func TestCommunityDigests(t *testing.T) {
 			s := &scanner.ScanStatus{}
 			s.Status = scanner.ScanStatusFinished
 			e := scans.NewEval()
+			committedAt := time.Now()
 			e.TranslatedResults = &scans.TranslatedResults{
 				Type: "community",
 				Data: scans.CommunityResults{
-					Committers: 123321,
+					Committers:  123321,
+					CommittedAt: committedAt,
 				},
 			}
 
 			ds, err := communityDigests(s, e)
 			Expect(err).To(BeNil())
-			Expect(len(ds)).To(Equal(1))
+			Expect(len(ds)).To(Equal(2))
 
 			Expect(ds[0].Title).To(Equal("unique committers"))
 			Expect(string(ds[0].Data)).To(Equal(`{"count":123321}`))
 			Expect(ds[0].Pending).To(BeFalse())
 			Expect(ds[0].Errored).To(BeFalse())
+
+			Expect(ds[1].Title).To(Equal("committed at"))
+			res := fmt.Sprintf("{\"chars\":\"%s\"}", committedAt.String())
+			Expect(string(ds[1].Data)).To(Equal(res))
 		})
 
 		g.It("should warn about single committer repos", func() {
@@ -49,7 +57,7 @@ func TestCommunityDigests(t *testing.T) {
 
 			ds, err := communityDigests(s, e)
 			Expect(err).To(BeNil())
-			Expect(len(ds)).To(Equal(1))
+			Expect(len(ds)).To(Equal(2))
 
 			Expect(ds[0].Title).To(Equal("unique committer"))
 			Expect(string(ds[0].Data)).To(Equal(`{"count":1}`))
