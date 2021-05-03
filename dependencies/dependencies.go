c9a2451406d0b7830cf2d45a4b69a9155b3918ac
Ian Ling <ian@iancaling.com>
2021-05-03 14:13:40 -0700

    Add OutdatedMeta to DependencyResolutionResponse

--- dependencies/dependencies.go
+++ dependencies/dependencies.go
@@ -1,6 +1,8 @@
 package dependencies
 
-import "time"
+import (
+	"time"
+)
 
 const (
 	// GetLatestVersionForDependencyEndpoint - returns the latest single version for a dependency
@@ -33,6 +35,14 @@ type Dependency struct {
 	Confidence    float32      `json:"confidence"`
 	CreatedAt     time.Time    `json:"created_at,omitempty"`
 	UpdatedAt     time.Time    `json:"updated_at,omitempty"`
+	OutdatedVersion OutdatedMeta `json:"outdated_version"`
+}
+
+// OutdatedMeta is used to represent the number of versions behind a dependency is
+type OutdatedMeta struct {
+	MajorBehind int `json:"major_behind" xml:"major_behind"`
+	MinorBehind int `json:"minor_behind" xml:"minor_behind"`
+	PatchBehind int `json:"patch_behind" xml:"patch_behind"`
 }
 
 // Meta represents all the known meta information for a dependency set
